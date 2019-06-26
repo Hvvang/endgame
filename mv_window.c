1094d889a5a222c5d0360495af5b64f77a9b7617
Maksym Libovych <mlibovych@e2r13p6.unit.ua>
2019-06-26 11:12:50 +0300

    move 1st enemy left to right

--- mv_window.c
+++ mv_window.c
@@ -4,9 +4,13 @@ void *mv_window(void* thread_data) {
     pthrData *data = (pthrData*) thread_data;
     int speed = 80000;
     int score = 0;
+    int next_j = 0;
+    int direction = 1;
     while(1) {
         
         for (int rn = 1; rn < 90; rn++) {
+	    int j = data->rand[rn];
+            int start_j = j;
             int set = (rn % 2) ? 0 : 2;
 //            wclear(data->win);
 //            wnoutrefresh(data->win2);
@@ -14,8 +18,8 @@ void *mv_window(void* thread_data) {
 //            wnoutrefresh(data->win2);
 //            doupdate();
             for (int i = 1; i < lines - lines / 10 - 1; i++) {
-
-                if(mx_iscreck(*data->x, *data->y, data->rand[rn], i)) {
+		int range = 8 + rand() % 40;
+                if(mx_iscreck(*data->x, *data->y, j, i)) {
                     pthread_cancel(thread);
                     clear();
                     refresh();
@@ -24,7 +28,14 @@ void *mv_window(void* thread_data) {
                 }
                 wclear(data->win);
                 wnoutrefresh(data->win);
-                mvwin(data->win, i, data->rand[rn]);	
+                //mvwin(data->win, i, data->rand[rn]);
+		mvwin(data->win, i, j);
+                next_j = j + direction;
+    	        if (next_j >= start_j + range || next_j < start_j - range || next_j < 2 || next_j >= cols - 12 ) {
+ 		    direction*= -1;
+ 		} else {
+ 		    j+= direction;
+ 		}	
                 window_print(data->win, 1, set);
                 wnoutrefresh(data->win);
                 doupdate();
